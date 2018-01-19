6bc772ee8859d5bab3b45b0c2edf10cba57263d5
Capril1042 <Caprilewis1042@gmail.com>
2018-01-19 18:41:55 -0500

    no strech

--- Answers.md
+++ Answers.md
@@ -1,1 +1,7 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
+1. Middleware- is used to connect network based requests  that are generated cilent-side, to the back end data that the cilent is requesting. 
+sessions- stores a session identifier on the cilent within a cookie.
+bcrypt- is an irreversible  password hashing function that is used to encrypt passwords
+JWT- Stands for JSON Web Token, a JSON web token is used for authenication. Once a user is signed in, requests made use this token allowing the user to access the routes permitted by that token. This way the user doesnt have to constantly sign in with their password to access these routes.
+2. In order to prevent attacks, bcrypt uses a salt to prevent rainbow table attacks, the number of salt rounds can be increased, so as ways to  decode get faster and better, so can the encrytion level. bcrypt is also an example of one way hashing, once the password is hashed that function is irreversible.
+3. The three parts of the JSON web token are; header, payload and signature
--- api/utils/middlewares.js
+++ api/utils/middlewares.js
@@ -26,6 +26,15 @@ const encryptUserPW = (req, res, next) => {
   // TODO: Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // Once the password is encrypted using bcrypt you'll need to set a user obj on req.user with the encrypted PW
   // Once the user is set, call next and head back into the userController to save it to the DB
+  bcrypt
+  .hash( password, SaltRounds)
+  .then((hash)=> {
+    req.user= {username, password:hash};
+    next();
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
 };
 
 const compareUserPW = (req, res, next) => {
@@ -35,6 +44,20 @@ const compareUserPW = (req, res, next) => {
   // You'll need to find the user in your DB
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
   // If the passwords match set the username on `req` ==> req.username = user.username; and call next();
+  User.findOne({ username }, (err, user )=> {
+    if (err) {
+    res.status(500).json({error:'User not found'});
+    return;
+    } 
+    bcrypt.compare(user.password, hash)
+    .then((matched) => {
+      req.username = user.username;
+      next();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    })
+    });
 };
 
 module.exports = {
